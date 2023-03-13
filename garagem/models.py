515d47ca00998b9a32d31b19f3853d2145102846
Raphael <Raphaelotto.RO@gmail.com>
2023-03-13 14:46:30 -0300

    Criação do modelo Categoria

--- garagem/models.py
+++ garagem/models.py
@@ -7,3 +7,8 @@ class Marca(models.Model):
     def __str__(self):
         return self.nome
 
+class Categoria(models.Model):
+    descricao = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.descricao
