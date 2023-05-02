e87b8d6f90dd93c36fd9c69cf7c68d9396300ece
Raphael-Otto <Raphaelotto.RO@gmail.com>
2023-05-02 15:40:15 -0300

    Commit de acessorios

--- garagem/serializers.py
+++ garagem/serializers.py
@@ -1,12 +1,11 @@
 from rest_framework.serializers import  ModelSerializer
 
-from garagem.models import Marca, Categoria, Cor
+from garagem.models import Marca, Categoria, Cor, Acessorio
 
 class MarcaSerializer(ModelSerializer):
     class Meta:
         model = Marca
         fields = "__all__"
-
 
 class CategoriaSerializer(ModelSerializer):
     class Meta:
@@ -16,4 +15,9 @@ class CategoriaSerializer(ModelSerializer):
 class CorSerializer(ModelSerializer):
     class Meta:
         model = Cor
+        fields = "__all__"
+
+class AcessorioSerializer(ModelSerializer):
+    class Meta:
+        model = Acessorio
         fields = "__all__"
