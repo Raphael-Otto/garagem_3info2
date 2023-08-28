5b92bb64cc74123b69c720ada5f4f3b515f57a72
Raphael-Otto <Raphaelotto.RO@gmail.com>
2023-08-28 13:21:58 -0300

    criacao de um modelo de dado

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "usuario",
+    "django_extensions",
     "drf_spectacular",
     "rest_framework",
     "uploader",
